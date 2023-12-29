9c471df18db7b58165b30d2245254d7645a1e3d1
Poorna Shri <poornashri2703@gmail.com>
2023-12-29 23:48:14 +0530

    task 5

---
+++ Day_4.py
@@ -0,0 +1,21 @@
+'''Problem Statement :
+time based greetings script run the script  run the script to get personalized greeting -good morning,good afternoon ,good nightbased on the current time'''
+
+#solution
+import datetime
+
+def get_greeting():
+    current_time = datetime.datetime.now().time()
+
+    if 5 <= current_time.hour < 12:
+        return "Good morning!"
+    elif 12 <= current_time.hour < 17:
+        return "Good afternoon!"
+    elif 17 <= current_time.hour <19:
+        return "Good evening!"
+    else:
+        return "Good night!"
+
+# Get and print the greeting
+greeting = get_greeting()
+print(greeting)
