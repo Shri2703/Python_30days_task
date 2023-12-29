57936df0e954d2bcef279516269ef50f5c3fa901
Poorna Shri <poornashri2703@gmail.com>
2023-12-29 23:49:44 +0530

    task 1

--- Day_1.py
+++ Day_1.py
@@ -2,6 +2,7 @@
 user should print the number from 1 to 100 the condition is 77,86,32 should not print  the series include 100 also ...
 '''
 #solution
+
 for i in range(1,101):
     if (i==77 or i== 86 or i == 32):
         continue 
